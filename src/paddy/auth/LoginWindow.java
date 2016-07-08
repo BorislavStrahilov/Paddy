d34961025d4ec996838e452ce117a5219cdaa06a
BorislavStrahilov <bstrah2@uic.edu>
2016-07-08 12:52:25 -0500

    Added BCrypt class and small updates to LoginWindow

--- src/paddy/auth/LoginWindow.java
+++ src/paddy/auth/LoginWindow.java
@@ -1,6 +1,9 @@
 package paddy.auth;
 
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -16,7 +19,7 @@ import paddy.main.TextPrompt;
 public class LoginWindow extends JFrame{
 	
 	private int windowWidth = 260;
-	private int windowHeight = 280;
+	private int windowHeight = 300;
 	
 	private int textFieldWidth = windowWidth - 45;
 	private int textFieldHeight = 30;
@@ -41,6 +44,12 @@ public class LoginWindow extends JFrame{
 	//JLabel to hold the logo
 	private JLabel logo;
 	
+	//JLabel to display general info
+	private JLabel infoLabel;
+	
+	//String to hold specific info to be displayed
+	private String infoText;
+	
 	//CONSTRUCTOR
 	public LoginWindow(){
 		
@@ -63,6 +72,7 @@ public class LoginWindow extends JFrame{
 		panel.add(registerButton);
 		
 		panel.add(logo);
+		panel.add(infoLabel);
 
 		setContentPane(panel);
 		getContentPane().setBackground(PaddyLauncher.mainCol);
@@ -74,9 +84,16 @@ public class LoginWindow extends JFrame{
 		logo = new JLabel(new ImageIcon("paddyLogo.png"));
 		logo.setBounds(20, 10, textFieldWidth, 100);
 		
+		//using HTML inside the label to allow for multiple lines
+		infoText = "Enter your credentials, or create an account.";
+		infoLabel = new JLabel("<html><div style='text-align: center;'>" + infoText + "</html>");
+		infoLabel.setBounds(20, (int) (windowHeight * 0.38), textFieldWidth, textFieldHeight);
+		infoLabel.setHorizontalAlignment(JLabel.CENTER);
+		infoLabel.setFont(PaddyLauncher.mainFont.deriveFont(Font.ITALIC, 12f));
+		
 		//user field creation and options
 		userField = new JTextField();
-		userField.setBounds(20, (int) (windowHeight * 0.45), textFieldWidth, textFieldHeight);
+		userField.setBounds(20, (int) (windowHeight * 0.50), textFieldWidth, textFieldHeight);
 		userField.setHorizontalAlignment(JTextField.CENTER);
 		userField.setFont(PaddyLauncher.mainFont);
 		userField.setForeground(PaddyLauncher.darkestCol);
@@ -110,14 +127,55 @@ public class LoginWindow extends JFrame{
 		loginButton = new JButton("Login");
 		loginButton.setBounds(userField.getX(), passwordField.getY() + textFieldHeight + 10, buttonWidth, buttonHeight);
 		loginButton.setBackground(PaddyLauncher.lighterCol);
-
+		loginButton.setFocusPainted(false);
+		
+		//action listener for login
+		loginButton.addActionListener(new ActionListener() { 
+			  public void actionPerformed(ActionEvent e) { 
+			    loginButtonPressed();
+			  } 
+		} );
+		
 		//create account button
 		registerButton = new JButton("Create");
 		registerButton.setBounds(loginButton.getX() + 10 + buttonWidth, passwordField.getY() + textFieldHeight + 10, buttonWidth, buttonHeight);
 		registerButton.setBackground(PaddyLauncher.lighterCol);
-
+		registerButton.setFocusPainted(false);
 		
+		//action listener for register
+		registerButton.addActionListener(new ActionListener() { 
+			  public void actionPerformed(ActionEvent e) { 
+			    registerButtonPressed();
+			  } 
+		} );
 
+	}
 
+	//user presses register button
+	protected void registerButtonPressed() {
+
+		//get password as a string
+		String pass = new String(passwordField.getPassword());
+		
+		String salt = BCrypt.gensalt();
+		String passHash = BCrypt.hashpw(pass, salt);
+		System.out.println(passHash);
+
+	}
+
+	//user presses login Button
+	protected void loginButtonPressed() {
+
+		//empty field check
+		if(userField.getText().equals("")){
+			infoText = "Both password and username field must be filled.";
+			infoLabel.setText("<html><div style='text-align: center;'>" + infoText + "</html>");
+			infoLabel.setForeground(Color.red);
+
+		}
+		
+		//get password as a string
+		String pass = new String(passwordField.getPassword());
+		
 	}
 }
